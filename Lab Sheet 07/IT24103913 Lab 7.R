1b507b97d4bd081bd4c5fed6095cf74ea2edd24e
IT24103913 <IT24103913@my.sliit.lk>
2025-09-17 20:37:15 +0530

    Lab 7

---
+++ Lab Sheet 07/IT24103913 Lab 7.R
@@ -0,0 +1,16 @@
+setwd ("C:\\Users\\kavee\\Desktop\\Lab 7")
+
+#Exercise
+#1
+(25 - 10) / (40 - 0)
+
+#2
+lambda <- 1/3
+pexp(2, rate = lambda)
+
+#3
+#i
+1 - pnorm(130, mean = 100, sd = 15)
+
+#ii
+qnorm(0.95, mean = 100, sd = 15)
